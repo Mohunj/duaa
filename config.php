f0aed905bfb296528886949b819ee4169124eb0a
Mohammed Alhoussainy <79022110+Mohunj@users.noreply.github.com>
2022-10-27 07:24:11 +0300

    Update config.php
    Fix issue

--- config.php
+++ config.php
@@ -1,8 +1,10 @@
 <?php
 //read duaa page ID from table wp_duaa
 
-$page_id= $wpdb->query($wpdb->prepare("SELECT 'duaa_page_id' FROM 'wp_duaa'"));
-
-//print ($page_id);
-
+$page_idd = $wpdb->prepare("SELECT duaa_page_id FROM wp_duaa WHERE id = %d", $id);
+          $results = $wpdb->get_results( $page_idd);
+          foreach ($results as $page_id_last) {
+            # code...
+            $duaa_id_last= $page_id_last->duaa_page_id;
+          }
 ?>
